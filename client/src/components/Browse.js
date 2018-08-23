8ba00b48f62dbe416d32692824092da65cc28bcf
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-22 19:38:05 -0500

    Created Browse component with initial styling

---
+++ client/src/components/Browse.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getSongs, getSong } from '../actions/songActions';
+import { getAlbum } from '../actions/albumActions';
+
+class Browse extends Component {
+  componentWillMount() {
+    this.props.getSongs();
+  }
+
+  onClick = (songId, album) => {
+    this.props.getSong(songId);
+    this.props.getAlbum(album);
+  }
+
+  render() {
+    const { songs } = this.props.songs;
+    // console.log(songs);
+
+    let songList = songs.map(song => (
+      <div className='grid-item' key={song._id}>
+        <span role='link' tabIndex='0' onClick={() => {this.onClick(song._id, song.album)}}>
+          <img src={song.artwork} alt='album cover'/>
+          <div className='grid-item-info'>
+            <p>{song.title}</p>
+          </div>
+        </span>
+      </div>
+    ));
+
+    return (
+      <div className='grid-container'>
+        {songList}
+      </div>
+    );
+  }
+}
+
+Browse.propTypes = {
+  getSongs: PropTypes.func.isRequired,
+  getSong: PropTypes.func.isRequired,
+  getAlbum: PropTypes.func.isRequired,
+  songs: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  songs: state.songs
+});
+
+export default connect(mapStateToProps, { getSongs, getSong, getAlbum })(Browse);
