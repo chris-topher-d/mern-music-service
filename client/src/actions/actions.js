ae82ada27d7c102f7699cb0e31085cdab386fcd9
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-03 15:29:39 -0500

    Updated redux actions

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_SONGS, GET_SONG, GET_ALBUM, GET_ARTIST, GET_ERRORS } from './types';
+import { GET_SONGS, GET_ALBUM, GET_ARTIST, SEARCH_FOR_ITEM, GET_ERRORS } from './types';
 
 // Get all songs
 export const getSongs = () => dispatch => {
@@ -7,22 +7,6 @@ export const getSongs = () => dispatch => {
     .then(res =>
       dispatch({
         type: GET_SONGS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
-}
-
-export const getSong = (id) => dispatch => {
-  axios.get(`/api/songs/${id}`)
-    .then(res =>
-      dispatch({
-        type: GET_SONG,
         payload: res.data
       })
     )
@@ -58,6 +42,25 @@ export const getArtist = artist => dispatch => {
     .then(res =>
       dispatch({
         type: GET_ARTIST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+// Search DB for matching terms
+export const searchFor = item => dispatch => {
+  console.log(item);
+  axios.get(`/api/search/${item}`)
+    .then(res =>
+      dispatch({
+        type: SEARCH_FOR_ITEM,
+        searchTerm: item,
         payload: res.data
       })
     )
