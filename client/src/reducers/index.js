f4db4bd4ddedb41fea01c73b7900c4e9a565b761
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-04 23:16:14 -0500

    Removed commented out code

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,62 @@
 import { combineReducers } from 'redux';
-import songReducer from './songReducer';
-import albumReducer from './albumReducer';
+import reduceReducers from 'reduce-reducers';
+// import songReducer from './songReducer';
+// import albumReducer from './albumReducer';
 import errorReducer from './errorReducer';
+import playlistReducer from './playlistReducer';
+import controlsReducer from './controlsReducer';
+import musicReducer from './musicReducer';
 
-export default combineReducers({
-  songs: songReducer,
-  album: albumReducer,
+const mainReducer = combineReducers({
+  playlists: musicReducer,
+  currentlyPlaying: playlistReducer,
+  controls: controlsReducer,
   errors: errorReducer
 });
+
+const createPlaylist = (state, action) => {
+  switch(action.payload) {
+    case 'browse':
+      return {
+        ...state,
+        currentlyPlaying: {
+          ...state.currentlyPlaying,
+          tracks: state.songs.tracks
+        }
+      };
+
+      case 'album':
+        return {
+          ...state,
+          currentlyPlaying: {
+            ...state.currentlyPlaying,
+            tracks: state.playlists.album.tracks
+          }
+        };
+
+      case 'artist':
+        return {
+          ...state,
+          currentlyPlaying: {
+            ...state.currentlyPlaying,
+            tracks: state.playlists.artist.tracks
+          }
+        };
+
+      case 'search':
+        return {
+          ...state,
+          currentlyPlaying: {
+            ...state.currentlyPlaying,
+            tracks: state.playlists.search.tracks
+          }
+        };
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = reduceReducers(createPlaylist, mainReducer);
+
+export default rootReducer;
