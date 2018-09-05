05b1e87b848b6ccebaaf259831b18a1d4bcbe757
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-04 23:24:07 -0500

    Filtered search results by category

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -12,84 +12,117 @@ class SearchResults extends Component {
     this.props.playSong(index);
   }
 
+  getAlbum = () => {
+
+  }
+
   render() {
     const displayStyle = {display: 'none'};
     const playingStyle = {background: '#f2f2f2'};
-    let index;
+    let index, songList, artistList, albumList;
     this.props.controls.index === null ? index = 0 : index = this.props.controls.index;
     const results = this.props.playlists.search.tracks;
 
-    const tracks = results.map((track, idx) => (
-      <li
-        key={track._id}
-        className='track-list-row'
-        style={this.props.currentlyPlaying.tracks[index] !== undefined && this.props.currentlyPlaying.tracks[index].title === track.title ? playingStyle : null}
-      >
-        <div className='track-count'>
-          <i className='fas fa-play' onClick={() => {this.playSong(idx, track.title)}}></i>
-          <span className='track-number'>{idx + 1}</span>
-        </div>
-        <div className='track-info'>
-          <span className='track-name'>{track.title}</span>
-          <span className='artist-name'>{track.artist}</span>
-        </div>
-        <div className='track-options'>
-          {/* <input type='hidden' className='song-id' value=''/>
-          <i className='fas fa-ellipsis-h'></i> */}
-        </div>
-        <div className='track-length'>
-          <span className='duration'>{track.duration}</span>
-        </div>
-      </li>
+    const tracks = this.props.playlists.search.tracks.filter(track => (
+      track.title.toLowerCase().includes(this.props.playlists.search.searchTerm.toLowerCase())
     ));
 
-    const albums = results.reduce((acc, next) => {
+    const artists = this.props.playlists.search.tracks.filter(track => (
+      track.artist.toLowerCase().includes(this.props.playlists.search.searchTerm.toLowerCase())
+    )).reduce((acc, next) => {
+      if (!acc.includes(next.artist)) acc.push(next);
+      return acc;
+    }, []);
+
+    const albums = this.props.playlists.search.tracks.filter(track => (
+      track.album.toLowerCase().includes(this.props.playlists.search.searchTerm.toLowerCase())
+    )).reduce((acc, next) => {
       if (acc.find(obj => obj.title === next.album) === undefined) {
-        acc.push({title: next.album, artwork: next.artwork});
+        acc.push({title: next.album, artist: next.artist, artwork: next.artwork});
       }
       return acc;
     }, []);
 
-    const albumList = albums.map((album, idx) => (
-      <div key={idx} className='grid-item'>
-        <Link to='/album'>
-          <span role='link' tabIndex='0' onClick={() => {this.playSong()}}>
-            <img src={album.artwork} alt='album cover'/>
-            <div className='grid-item-info'>
-              {album.title}
-            </div>
+    // console.log(albums);
+
+    if (tracks.length > 0) {
+      songList = tracks.map((track, idx) => (
+        <li
+          key={track._id}
+          className='track-list-row'
+          style={this.props.currentlyPlaying.tracks[index] !== undefined && this.props.currentlyPlaying.tracks[index].title === track.title ? playingStyle : null}
+        >
+          <div className='track-count'>
+            <i className='fas fa-play' onClick={() => {this.playSong(idx, track.title)}}></i>
+            <span className='track-number'>{idx + 1}</span>
+          </div>
+          <div className='track-info'>
+            <span className='track-name'>{track.title}</span>
+            <span className='artist-name'>{track.artist}</span>
+          </div>
+          <div className='track-options'>
+            {/* <input type='hidden' className='song-id' value=''/>
+            <i className='fas fa-ellipsis-h'></i> */}
+          </div>
+          <div className='track-length'>
+            <span className='duration'>{track.duration}</span>
+          </div>
+        </li>
+      ));
+    } else {
+      songList = <span className='no-search-results'>No song titles containing <i><strong>{this.props.playlists.search.searchTerm}</strong></i></span>;
+    }
+
+    if (artists.length > 0) {
+      artistList = artists.map((artist, idx) => (
+        <div key={idx} className='artist-result-row' role='link' tabIndex='0' onClick={() => {this.playSong()}}>
+          <span className='artist-info' style={this.props.playlists.search.tracks.length > 0 ? null : displayStyle}>
+            <span className='artist-name'>{artist.artist}</span>
           </span>
-        </Link>
-      </div>
-    ));
+        </div>
+      ));
+    } else {
+      artistList = <span className='no-search-results'>No artist names containing <i><strong>{this.props.playlists.search.searchTerm}</strong></i></span>;
+    }
+
+    if (albums.length > 0 ) {
+      albumList = albums.map((album, idx) => (
+        <div key={idx} className='grid-item'>
+          <Link to='/album'>
+            <span role='link' tabIndex='0' onClick={() => {this.getAlbum()}}>
+              <img src={album.artwork} alt='album cover'/>
+              <div className='grid-item-info'>
+                <p className='album-title'>{album.title}</p>
+                <p className='album-artist'>{album.artist}</p>
+              </div>
+            </span>
+          </Link>
+        </div>
+      ));
+    } else {
+      albumList = <span className='no-search-results'>No album titles containing <i><strong>{this.props.playlists.search.searchTerm}</strong></i></span>;
+    }
 
     return (
       <div className='search-container'>
         <div className='search-results'>
+          <h2>Search Results for <i>{this.props.playlists.search.searchTerm}</i></h2>
           <div className='tracks-container'>
             <ul className='track-list'>
               <span className='track-list-title'><h2>Tracks</h2></span>
-              <span className='no-results' style={this.props.playlists.search.tracks.length > 0 ? displayStyle : null}>No song titles containing <i>{this.props.playlists.search.searchTerm}</i></span>
-              {tracks}
+              {songList}
             </ul>
           </div>
 
           <div className='artist-results'>
             <div className='artist'>
               <h2>Artists</h2>
-              <span className='no-results' style={this.props.playlists.search.tracks.length > 0 ? displayStyle : null}>No artists found for <i>{this.props.playlists.search.searchTerm}</i></span>
-              <div className='artist-result-row' role='link' tabIndex='0' onClick={() => {this.playSong()}}>
-                <span className='artist-info' style={this.props.playlists.search.tracks.length > 0 ? null : displayStyle}>
-                  <span className='artist-name'>{this.props.playlists.search.tracks.length > 0 ? results[0].artist : null}</span>
-                  <p className='album-track-count'>Albums: {albums.length}, Tracks: {tracks.length}</p>
-                </span>
-              </div>
+              {artistList}
             </div>
           </div>
 
           <div className='album-results'>
             <h2>Albums</h2>
-            <span className='no-results' style={this.props.playlists.search.tracks.length > 0 ? displayStyle : null}>No album titles containing <i>{this.props.playlists.search.searchTerm}</i></span>
             <div className='albums'>
               {albumList}
             </div>
