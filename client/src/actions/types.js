59140bd562bf8341e81ae8ddcaa83df332a0092c
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-05 18:39:54 -0500

    Added playlist action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,14 @@
 export const GET_SONGS = 'GET_SONGS';
 export const GET_SONG = 'GET_SONG';
 export const GET_ALBUM = 'GET_ALBUM';
+export const GET_ARTIST = 'GET_ARTIST';
 // export const GET_ALBUM_INFO = 'GET_ALBUM_INFO';
 export const GET_ERRORS = 'GET_ERRORS';
+export const LOAD_PLAYLIST = 'LOAD_PLAYLIST';
+export const GET_PLAYLISTS = 'GET_PLAYLISTS';
+export const GET_PLAYLIST = 'GET_PLAYLIST';
+export const CREATE_PLAYLIST = 'CREATE_PLAYLIST';
+export const PLAY_SONG = 'PLAY_SONG';
+export const UPDATE_INDEX = 'UPDATE_INDEX';
+export const SET_PLAY = 'SET_PLAY';
+export const SEARCH_FOR_ITEM = 'SEARCH_FOR_ITEM';
