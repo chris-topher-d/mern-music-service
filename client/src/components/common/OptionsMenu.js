6ad896e3663ddeb05924e19f46b260c8ddc75cfc
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-09 19:05:07 -0500

    Updated on click animation

--- client/src/components/common/OptionsMenu.js
+++ client/src/components/common/OptionsMenu.js
@@ -4,20 +4,31 @@ import PropTypes from 'prop-types';
 import { getPlaylists, addToPlaylist } from '../../actions/playlistActions';
 
 class OptionsMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    }
+  }
+
   componentWillMount() {
     this.props.getPlaylists();
   }
 
   addToPlaylist = (playlistId, songId) => {
+    this.setState({clicked: playlistId});
     this.props.addToPlaylist(playlistId, songId);
   }
 
   render() {
+    const { clicked } = this.state;
+
     const playlists = this.props.music.playlists.map(playlist => (
       <p
         key={playlist._id}
-        className='playlist'
+        className={clicked === playlist._id ? 'playlist clicked' : 'playlist'}
         onClick={() => {this.addToPlaylist(playlist._id, this.props.songId)}}
+        onAnimationEnd={() => this.setState({clicked: false})}
       >
         {playlist.title}
       </p>
