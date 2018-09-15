c99ed40b6117a508a3cda5609080b1dcdfd16f64
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-14 21:21:29 -0500

    Removed song repeat action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,20 +1,3 @@
-// export const GET_SONGS = 'GET_SONGS';
-// export const GET_ALBUM = 'GET_ALBUM';
-// export const GET_ARTIST = 'GET_ARTIST';
-// export const GET_ERRORS = 'GET_ERRORS';
-// export const LOAD_PLAYLIST = 'LOAD_PLAYLIST';
-// export const GET_PLAYLISTS = 'GET_PLAYLISTS';
-// export const GET_PLAYLIST = 'GET_PLAYLIST';
-// export const CREATE_PLAYLIST = 'CREATE_PLAYLIST';
-// export const DELETE_PLAYLIST = 'DELETE_PLAYLIST';
-// export const ADD_TO_PLAYLIST = 'ADD_TO_PLAYLIST';
-// export const REMOVE_FROM_PLAYLIST = 'REMOVE_FROM_PLAYLIST';
-// export const PLAY_SONG = 'PLAY_SONG';
-// export const UPDATE_INDEX = 'UPDATE_INDEX';
-// export const SET_PLAY = 'SET_PLAY';
-// export const SEARCH_FOR_ITEM = 'SEARCH_FOR_ITEM';
-// export const PAGE_LOADING = 'PAGE_LOADING';
-
 // Music Action Types
 export const GET_SONGS = 'GET_SONGS';
 export const GET_ALBUM = 'GET_ALBUM';
