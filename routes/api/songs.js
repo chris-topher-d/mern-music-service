86e7fc434b79c4d679cb7bcdeaa0d3b23fb52de3
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-20 15:23:37 -0500

    Commented out POST route - for development only

--- routes/api/songs.js
+++ routes/api/songs.js
@@ -9,27 +9,40 @@ const Song = require('../../models/Song');
 // @desc   Get all songs
 // @access Public
 router.get('/', (req, res) => {
-  // res.json({msg: 'Show all songs'});
   Song.find()
     .sort({album: 1})
-    .then(songs => res.json(songs))
+    // .then(songs => res.json(songs))
+    .then(songs => {
+      // Create a randomized array
+      let randomizedOrder = [];
+      while (randomizedOrder.length !== songs.length) {
+        let index = Math.floor(songs.length * Math.random());
+        if (randomizedOrder.find(track => track.id === songs[index].id) === undefined) {
+          randomizedOrder.push(songs[index]);
+        }
+      }
+      return res.json(randomizedOrder.slice(0, 10));
+    })
     .catch(err => res.status(404).json({nosongs: 'No songs found'}));
 });
 
-router.post('/', (req, res) => {
-  const newSong = new Song({
-    title: req.body.title,
-    artist: req.body.artist,
-    album: req.body.album,
-    artwork: req.body.artwork,
-    genre: req.body.genre,
-    duration: req.body.duration,
-    path: req.body.path,
-    albumOrder: req.body.albumOrder
-  });
-
-  newSong.save().then(song => res.json(song));
-});
+// @route POST api/songs
+// @desc Add a new song
+// For Development
+// router.post('/', (req, res) => {
+//   const newSong = new Song({
+//     title: req.body.title,
+//     artist: req.body.artist,
+//     album: req.body.album,
+//     artwork: req.body.artwork,
+//     genre: req.body.genre,
+//     duration: req.body.duration,
+//     path: req.body.path,
+//     albumOrder: req.body.albumOrder
+//   });
+//
+//   newSong.save().then(song => res.json(song));
+// });
 
 // @route  GET api/songs
 // @desc   Get song by ID
