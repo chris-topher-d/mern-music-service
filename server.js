2d0a5ca578b40e75e9134b2685a317a96fd0a64f
Christopher Dennis <cdennis.aus@gmail.com>
2018-09-22 23:24:17 -0500

    Included static assets for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const songs = require('./routes/api/songs');
 const album = require('./routes/api/album');
@@ -29,6 +30,16 @@ app.use('/api/album', album)
 app.use('/api/artist', artist);
 app.use('/api/search', search);
 app.use('/api/playlists', playlist);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
